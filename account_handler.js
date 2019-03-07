72e26d4b0d0a780b51af86a884e8287739774524
danielbang907 <43049538+danielbang907@users.noreply.github.com>
2019-03-06 22:07:59 -0600

    Add files via upload

---
+++ account_handler.js
@@ -0,0 +1,66 @@
+let num1 = 6;
+let num2 = 10;
+let accounts = [
+  {
+    "name": "Mr. Finance",
+    "email": 'master@financefriend.com',
+    "password": 'financefriend',
+    "streak": num1,
+  },
+  {
+    "name": "Mrs. Finance",
+    "email": "master2@financefriend.com",
+    "password": "financefriend2",
+    "streak": num2,
+  }
+];
+
+function get_profile(){
+  let account = {
+    "email": $('#email').val(),
+    "password": $('#password').val(),
+  };
+  accounts.push(account);
+}
+
+function account_checker(){
+  let email = $('#email').val();
+  let password = $('#password').val();
+  for(var i = 0; i < accounts.length; i++){
+    if(accounts[i].email == email && accounts[i].password == password) {
+      return true;
+    }
+  }
+  return false;
+}
+
+$('form[name=login-form]').submit(function(e) {
+  if(!account_checker()) {
+    return false;
+  }
+});
+
+$('form[name=landing-form]').submit(function(e) {
+  if($('#check-password').val() != $('#password').val()){
+    window.alert("Error: Passwords do not match!\nPassword = " + $('#password').val() + "\nConfirm Password = " + $('#check-password').val());
+    return false;
+  }
+  window.alert("New account created!\nEmail = " + $('#email').val() + "\nPassword = " +  $('#password').val());
+});
+
+$('#user-name').ready(() => {
+  $('#user-name').html(accounts[0].name);
+})
+
+$('#streak').ready(() => {
+  $('#streak').html(accounts[0].streak);
+})
+
+
+function add_streak(){
+  window.alert("Congratulations!\nBecause you used Finance Friend today,\nYour streak increased by 1!")
+  num1 = 7;
+  $('#streak').ready(() => {
+    $('#streak').html(7);
+  })
+}
